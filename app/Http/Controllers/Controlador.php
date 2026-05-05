cb4352dd468e801bd73894a385ae5ebbabeb2cf5
jimmy08082005 <e.jaimerj@go.ugr.es>
2026-05-05 20:00:57 +0200

    Añadida ruta a Iniciar Sesión

--- app/Http/Controllers/Controlador.php
+++ app/Http/Controllers/Controlador.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class Controlador extends Controller
 {
-    
+    public function login() {
+        return view('iniciarsesion');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Controlador;
+
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/login', [Controlador::class, 'login']);
