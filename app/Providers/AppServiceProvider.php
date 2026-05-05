f4de44a35b3cdb69c5754b92d9ca1eefaf464b61
jimmy08082005 <e.jaimerj@go.ugr.es>
2026-05-05 16:28:54 +0200

    Modificado error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
+public function boot(): void
+{
+    if (app()->environment('production') || env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+}
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
