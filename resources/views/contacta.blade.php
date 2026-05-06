5cc88f858708aa1834e76324656f4a1c9ba76077
Sr.Rosales <alexroscan@correo.ugr.es>
2026-05-06 17:18:59 +0200

    footer.blade.php aniadido

--- resources/views/contacta.blade.php
+++ resources/views/contacta.blade.php
@@ -65,10 +65,6 @@
         <p class="mb-1">Este sitio web y su contenido están protegidos por derechos de autor.</p>
         <a href="/contacta.html">Contacta</a>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ resources/views/util/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center py-3">
+        <p class="mb-1">© 2026. Todos los derechos reservados.</p>
+        <p class="mb-1">Este sitio web y su contenido están protegidos por derechos de autor.</p>
+        <a href="/contacta.html">Contacta</a>
+    </footer>
