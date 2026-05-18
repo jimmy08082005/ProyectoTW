7543a62344b26b67f5133bbdfe0d05bdc85df3ea
Sr.Rosales <alexroscan@correo.ugr.es>
2026-05-18 11:51:07 +0200

    pdf sobre como se hizo la web añadido

--- resources/views/util/footer.blade.php
+++ resources/views/util/footer.blade.php
@@ -1,5 +1,8 @@
 <footer class="container-fluid text-center py-3">
     <p class="mb-1">© 2026. Todos los derechos reservados.</p>
     <p class="mb-1">Este sitio web y su contenido están protegidos por derechos de autor.</p>
-    <a clas="enlace-contacta"href="/contacta">Contacta</a>
+    <a class="enlace-contacta me-3" href="/contacta">Contacta</a>
+    <a href="{{ asset('doc/A1_2 - Directorio de Rutas de Senderismo y Turismo.pdf') }}" target="_blank">
+        PDF de como se hizo la web
+    </a>
 </footer>
